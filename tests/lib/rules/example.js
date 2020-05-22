122dcc2f145c817e68cf1809b7614ce2340c65bd
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-05-22 17:02:44 +0200

    test: write rule testcases

--- tests/lib/rules/example.js
+++ tests/lib/rules/example.js
@@ -1,16 +1,73 @@
 'use strict';
 
-const rule = require('../../../lib/rules/example');
+const { flatMap } = require('../../../lib/utils');
+const rule = require('../../../lib/rules/noTrivialAwait');
 const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester();
-ruleTester.run('example', rule, {
-    valid: ['var validExample;', 'for(;;) {}', 'do {} while(condition)'],
+const parserOptions = { ecmaVersion: 8 };
+const errors = [{ messageId: 'avoidTrivialAwait' }];
 
-    invalid: [
-        {
-            code: 'while(condition) {}',
-            errors: [{ messageId: 'avoidWhileLoops' }],
-        },
-    ],
+const validFunctionBodies = [
+    `const a = await fetch();
+     const b = await fetch();
+     const c = a + b;`,
+    `const [a] = await fetch();
+     const b = await fetch();
+     const c = [a, b];`,
+    `const { a } = await fetch();
+     const b = await fetch();
+     return { a, b };`,
+    `const a = await fetch();
+     const b = await fetch();
+     return add(a, b);`,
+];
+
+const invalidFunctionBodies = [
+    `return await fetch();`,
+    `const a = await fetch();
+   return a;`,
+    `const a = await fetch();
+   cosnt b = await fetch();
+   return b;`,
+    `const a = await fetch();
+   cosnt b = await fetch(a);
+   return b;`,
+    `const a = await fetch();
+   const c = a + 1;
+   cosnt b = await fetch();
+   return b;`,
+];
+
+const createFunctionDeclarations = body => [
+    `async () => {
+      ${body}
+    }`,
+    `async function fn() {
+      ${body}
+    }`,
+    `class A {
+      async fn() {
+        ${body}
+      }
+    }`,
+];
+
+const validFunctionDeclarations = flatMap(
+    createFunctionDeclarations,
+    validFunctionBodies,
+);
+
+const invalidFunctionDeclarations = flatMap(
+    createFunctionDeclarations,
+    invalidFunctionBodies,
+);
+
+ruleTester.run('noTrivialAwait', rule, {
+    valid: validFunctionDeclarations.map(code => ({ code, parserOptions })),
+    invalid: invalidFunctionDeclarations.map(code => ({
+        code,
+        parserOptions,
+        errors,
+    })),
 });
